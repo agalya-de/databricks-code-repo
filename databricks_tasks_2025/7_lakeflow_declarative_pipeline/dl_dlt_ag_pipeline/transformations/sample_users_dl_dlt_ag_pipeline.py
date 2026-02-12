13a1dd89bfb726c26e173cd086c6a1fa0a02bc60
agalya-de <agalya.dotnet.2321@gmail.com>
2026-02-12 12:43:34 +0000

    pl_dp_dlt_pipeline_task_12Feb26

--- databricks_tasks_2025/7_lakeflow_declarative_pipeline/dl_dlt_ag_pipeline/transformations/sample_users_dl_dlt_ag_pipeline.py
+++ databricks_tasks_2025/7_lakeflow_declarative_pipeline/dl_dlt_ag_pipeline/transformations/sample_users_dl_dlt_ag_pipeline.py
@@ -1,8 +1,6 @@
-from pyspark import Pipelines as dp
+from pyspark import pipelines as dp
 
-import dlt
-
-@dlt.table()#4.store the final result into the table in the name of the function
+@dp.table()#4.store the final result into the table in the name of the function
 def shipments1():#1.create a function
     df=spark.read.table("prodcatalog_wd36.logistics_wd36.bronze_shipments1")#2.write any pyspark program
     return df#3. return the final result
